df5b24fca5d9fa4e817b6fefd49da3f297b23f73
Klaster <107881924+TheGodsHand777@users.noreply.github.com>
2023-03-03 22:51:30 +0100

    Update SingularityItems.java

--- src/singularity/content/SingularityItems.java
+++ src/singularity/content/SingularityItems.java
@@ -1,16 +1,16 @@
 package singularity.content;
 
-import arc.graphics.*;
-import mindustry.type.*;
+import arc.graphics.Color;
+import mindustry.type.Item;
 
 public class Items {
-     public static Item omegaSingularity;
+    public static Item omegaSingularity;
 
-    public static void load(){
-        omega-singularity = new Item("omegaSingularity", Color.valueOf("ffffff")){{
+    public static void load() {
+        omegaSingularity = new Item("omegaSingularity", Color.valueOf("ffffff")) {{
             flammability = 100.0f;
             radioactivity = 100.0f;
-            explosiveness = 100.0;
+            explosiveness = 100.0f;
             charge = 100.0f;
             cost = 100.0f;
             hardness = 100;
