7bc9163df952b8fb3316c7062f91c5f663dc6857
Klaster <107881924+TheGodsHand777@users.noreply.github.com>
2023-03-04 00:18:58 +0100

    Update SingularityMod.java

--- src/singularity/SingularityMod.java
+++ src/singularity/SingularityMod.java
@@ -14,19 +14,6 @@ public class SingularityMod extends Mod{
 
     public SingularityMod(){
         Log.info("Loaded SingulaityMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("singularity");
-                dialog.cont.add("Pray for the miracles...").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("singularity-mod-omega-singularity")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
     }
 
     @Override
