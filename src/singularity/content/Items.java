5fc6dfedc79cf5bf272edb01b81852e300b639a8
Klaster <107881924+TheGodsHand777@users.noreply.github.com>
2023-03-03 22:01:39 +0100

    Update and rename src/singularity/EIItems.java to src/singularity/content/Items.java

---
+++ src/singularity/content/Items.java
@@ -0,0 +1,19 @@
+package singularity.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class Items {
+     public static omega-singularity;
+
+    public static void load(){
+        omegaSingularity = new Item("omega-singularity", Color.valueOf("ffffff")){{
+            flammability = 100.0f;
+            radioactivity = 100.0f;
+            explosiveness = 100.0;
+            charge = 100.0f;
+            cost = 100.0f;
+            hardness = 100;
+        }};
+    }
+}
