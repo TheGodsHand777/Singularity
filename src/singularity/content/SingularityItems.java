92a3b9bbfa25af4b34d5653f298e4edb3a26e972
Klaster <107881924+TheGodsHand777@users.noreply.github.com>
2023-03-04 09:59:22 +0100

    Add files via upload

---
+++ src/singularity/content/SingularityItems.java
@@ -0,0 +1,19 @@
+package singularity.content;
+
+import arc.graphics.Color;
+import mindustry.type.Item;
+
+public class SingularityItems {
+    public static Item omegaSingularity;
+
+    public static void load() {
+        omegaSingularity = new Item("omegaSingularity", Color.valueOf("ffffff")) {{
+            flammability = 100.0f;
+            radioactivity = 100.0f;
+            explosiveness = 100.0f;
+            charge = 100.0f;
+            cost = 100.0f;
+            hardness = 100;
+        }};
+    }
+}
